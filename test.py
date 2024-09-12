ff0f46f039ac99c3d50587c1ac3fa08be4ed12c8
User <nazaroruslan04@gmail.com>
2024-09-12 21:20:45 +0400

    Add file

--- test.py
+++ test.py
@@ -2,8 +2,13 @@ def first_function(var_1, var_2, var_3):
  return var_1* 0.5 + var_2/3 + var_3 ** 4.3
 function_res_1 = first_function(1, 2, 3)
 print(function_res_1)
-function_res_2 = first_function(var_1=1, var_3=3,
- var_2=2)
+function_res_2 = first_function(var_1=1, var_3=3, var_2=2)
 print(function_res_2)
 function_res_3 = first_function(1, 2, var_3=3)
 print(function_res_3)
+
+def appender(var_1, list_1 = []):
+ list_1.append(var_1)
+ return list_1
+first = appender(1)
+print(first)
